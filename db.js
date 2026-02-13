e62457b711a19444b0ee9b15db150f1ea9689156
Bhavya Bandlamudi <139468266+bhavyabandlam@users.noreply.github.com>
2026-02-13 20:23:03 +0530

    Update db.js to use environment variables
    Removed database connection module and added environment variables for database configuration.

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
-module.exports = {
-  connect: () => {
-    console.log("User Service DB connected");
-  }
-};
-
+DB_HOST=database-1.c0lkyewi6qn3.us-east-1.rds.amazonaws.com
+DB_USER=admin
+DB_PASSWORD=xxX9PGVn2yLb7La8vrFx
+DB_NAME=database-1
+DB_PORT=3306
